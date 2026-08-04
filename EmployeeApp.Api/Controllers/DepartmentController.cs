5d249cff054598b0835b699c8fadc6ef6ea1fb13
reightenfels <reightenfels@mail.ru>
2026-08-04 12:49:57 +0300

    fix: validate department id

--- EmployeeApp.Api/Controllers/DepartmentController.cs
+++ EmployeeApp.Api/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 using EmployeeApp.Core.DTO.Department;
 using EmployeeApp.Core.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using EmployeeApp.Core.Exceptions;
 
 namespace EmployeeApp.Api.Controllers;
 
@@ -22,6 +23,11 @@ public class DepartmentController : ControllerBase
     [ProducesResponseType(typeof(IEnumerable<UpdateSalaryResultDTO>), StatusCodes.Status200OK)]
     public async Task<ActionResult<IEnumerable<UpdateSalaryResultDTO>>> UpdateDepartmentSalary(long id, [FromBody] UpdateSalaryDTO dto)
     {
+        if (await _departmentService.IsExists(id) is false)
+        {
+            throw new ResourceNotFoundException($"Не найден отдел с ID: {id}");
+        }
+
         IEnumerable<UpdateSalaryResultDTO> result = await _departmentService.UpdateSalaryForDepartment(id, dto.Percent);
         return Ok(result);
     }

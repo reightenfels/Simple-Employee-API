8ca8f9f757a561dd2d5d7059d4401fc462344893
reightenfels <reightenfels@mail.ru>
2026-08-04 10:27:44 +0300

    add: allow cross origin

--- EmployeeApp.Api/Program.cs
+++ EmployeeApp.Api/Program.cs
@@ -46,6 +46,16 @@ try
         c.IncludeXmlComments(xmlPath);
     });
 
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAll", policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+    });
+
     var app = builder.Build();
 
     if (app.Environment.IsDevelopment())
@@ -57,6 +67,8 @@ try
             c.RoutePrefix = string.Empty;
         });
     }
+
+    app.UseCors("AllowAll");
 
     app.UseHttpsRedirection();
 
